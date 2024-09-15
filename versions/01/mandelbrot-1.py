5f5bd921bfe5a51c7cf4f69673946a4009038fcf
Martin <tino@mbucher.ch>
2024-09-15 18:46:46 +0200

    branch 04 erstellt

--- versions/01/mandelbrot-1.py
+++
@@ -1,9 +0,0 @@
-def mandelbrot(z0:complex, orbits:int=255) -> int:
-    z = z0
-    for n in range(orbits):
-        if abs(z) > 2.0:
-            return n
-        z = z * z + z0
-        
-if __name__ == '__main__':
-    print(mandelbrot(0.0965-0.638j))  # Should print 17
